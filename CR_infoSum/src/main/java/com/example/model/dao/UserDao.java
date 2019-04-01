efe320076ed4f685f21961bf53e5c2004a0958b4
Winneryang <823713793@qqcom>
2019-04-02 00:16:59 +0800

    今晚有点晚了，把数据库设计了一下，dao层初步构思了一下,把注释模板做了一下

--- CR_infoSum/src/main/java/com/example/model/dao/UserDao.java
+++ CR_infoSum/src/main/java/com/example/model/dao/UserDao.java
@@ -1,10 +1,21 @@
 package com.example.model.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.example.model.bean.User;
 
+
+
+
+/** @version:
+* @Description: 
+* @Param :
+* @author: Winner杨 
+* @date:2019年4月2日上午12:15:03  
+*/ 
 @Mapper
 public interface UserDao {
 	//验证手机号和密码是否一致
@@ -12,4 +23,20 @@ public interface UserDao {
 	
 	//查询该手机是否被注册
 	public User findPhone(@Param("phone")Integer phone);
+	
+	//company的全部信息和算出各个时间的时间差根据时间差排序从大到小
+	List<company> queryAll(@Param("offset") int offset,@Param("limit") int limit);
+	
+	//插入一条数据
+	
+	
+	/**
+	 * @author Administrator
+	 * 查询某个公司的排行榜 
+	 *
+	 */ 
+	
+	//查询某个岗位的排行榜
+	
+	//
 }
